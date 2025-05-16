fad9969909a0cb17bd4c3b98ff5dce651ced73cf
gy-mag <snagi.sh1@gmail.com>
2025-05-16 15:41:32 +0900

    nagi

--- Counter.java
+++ Counter.java
@@ -2,11 +2,11 @@ public class Counter {
     public void run() {
         for (int i = 1; i <= 15; i++) {
             if (new FizzBuzzChecker().isFizzBuzz(i)) {
-                System.out.println("fizzbuzz");
+                System.out.println("Nagi");
             } else if (new FizzChecker().isFizz(i)) {
-                System.out.println("fizz");
+                System.out.println("Nagi");
             } else if (new BuzzChecker().isBuzz(i)) {
-                System.out.println("buzz");
+                System.out.println("Nagi");
             } else {
                 System.out.println(i);
             }
